810ca9b09c37e5fb77cd78efb07184adb2ba6f2b
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-07-19 09:09:35 +0200

    modified:   src/Components/About/slices/OurHistory.jsx 	modified:   src/output.css

--- src/Components/About/slices/OurHistory.jsx
+++ src/Components/About/slices/OurHistory.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import photo2 from '../../../imgs/Photo-history.jpg'
+import { RiPlayLargeFill } from "react-icons/ri";
 
 export default function OurHistory() {
     
@@ -15,8 +16,17 @@ export default function OurHistory() {
 <div className='relative  mt-20'>        
      <div className="relative z-50">
         <div className="relative">
-            <div className=' after:content-[""] after:z-20 after:bg-[url("./imgs/Pattern.svg")] after:bg-cover after:absolute after:w-60 after:bg-no-repeat after:h-60 after:-top-8 after:-right-0 after:transform      before:content-[""] before:z-30  before:bg-cover before:absolute before:w-[126px] before:h-[113px] before:rounded-2xl before:bg-[#4EAF4E] before:-bottom-28 before:-left-24 before:transform before:translate-x-1/2 before:-translate-y-20'>
-                <img src={photo2} alt="she explaines to her something" className="rounded-special relative z-50" />
+            <div className=' relative after:content-[""] after:z-20 after:bg-[url("./imgs/Pattern.svg")] after:bg-cover after:absolute after:w-60 after:bg-no-repeat after:h-60 after:-top-8 after:-right-0 after:transform      before:content-[""] before:z-30  before:bg-cover before:absolute before:w-[126px] before:h-[113px] before:rounded-2xl before:bg-[#4EAF4E] before:-bottom-28 before:-left-24 before:transform before:translate-x-1/2 before:-translate-y-20'>
+                <img src={photo2} alt="she explaines to her something" className=" rounded-special w-full relative z-40" />
+                <div className=' flex items-center justify-center bg-black/50 absolute top-0 left-0 right-0 bottom-0 rounded-special z-50'>
+                     <div className=' flex items-center justify-center border-[.3px]  rounded-full border-[#FFFFFF6B] bg-[#FFFFFF14] w-[109px] h-[109px]'>
+                        <div className='flex items-center justify-center w-[87px] h-[87px] rounded-full bg-[#FFFFFF2B] border-[.3px] border-[#D0D0D0]'>
+                            <div className='flex items-center justify-center cursor-pointer w-[67px] h-[67px] rounded-full bg-white group/play '>
+                                 <RiPlayLargeFill className=' text-[#3B3A5D] text-3xl group-hover/play:text-[#ff1f1f] transition-all duration-300'/>
+                            </div>
+                        </div>
+                     </div>
+                </div>
             </div>
         </div>
         
